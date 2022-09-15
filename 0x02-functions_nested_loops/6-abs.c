f99fd371a172e28a0fd78269eb875755a3afc047
Killian Nthakyo <killynthakyo@gmail.com>
2022-09-14 18:17:17 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+/**
+ * _abs - entry point for the program.
+ *
+ * @c : character passed as c.
+ *
+ * Return:int ret
+ */
+int _abs(int c)
+{
+	int ret = abs(c);
+
+	return (ret);
+}
