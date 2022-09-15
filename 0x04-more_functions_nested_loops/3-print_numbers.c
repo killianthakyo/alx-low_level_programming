cc8549c4c696be521d1872470f2646ad8b1a0e33
Killian Nthakyo <killynthakyo@gmail.com>
2022-09-15 04:17:24 -0700

    Print numbers from 0 to 9 with putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point for the program.
+ *
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
