f74a31cab4d5ada4d52ea0ce69620df86c5235e4
Killian Nthakyo <killynthakyo@gmail.com>
2022-09-19 19:46:18 -0700

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - entry point for the program.
+ * @s: A pointer Parameter 1  passed from main
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
+
+	while (s[fcounter] != '\0')
+		fcounter++;
+
+	for (i = 0; i < fcounter; i++)
+	{
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
+	}
+}
