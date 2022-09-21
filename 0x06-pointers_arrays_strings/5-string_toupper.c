22a79f753375580e180fc1fb91964e27644d7720
Killian Nthakyo <killynthakyo@gmail.com>
2022-09-21 12:37:22 -0700

    String to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - reverse array
+ * @str: param1
+ *
+ * Return void
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
