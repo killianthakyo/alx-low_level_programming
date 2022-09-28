678effe9a7e4eb090b742aa264ef8564d5aa7aee
Killian Nthakyo <killynthakyo@gmail.com>
2022-09-28 06:49:40 -0700

    Function to get square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,11 +20,11 @@ int _sqrt_recursion(int n)
  */
 int _find_sqrt(int i, int n)
 {
+	if (i < 1)
+                return (-1);
 	if (n == 1)
 		return (1);
 	if ((i * i) == n)
 		return (i);
-	if ((i * i) < n)
-		return (-1);
 	return (_find_sqrt(i - 1, n));
 }
