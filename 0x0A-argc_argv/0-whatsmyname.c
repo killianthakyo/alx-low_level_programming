e555b9e8bf5de8489db0e9766c2f140b8d75feee
Killian Nthakyo <killynthakyo@gmail.com>
2022-10-05 06:59:14 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: Argument count.
+ * @argv: Parameters.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
