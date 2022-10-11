67982e9adbb200438cf3908b880a77caa08ee700
Killian Nthakyo <killynthakyo@gmail.com>
2022-10-11 13:40:41 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - Structure dog
+ * @name: DOg's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char * owner;
+}
+
+
+
+#endif /* MAIN_H */
